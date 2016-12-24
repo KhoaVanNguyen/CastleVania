44676ce15406fcd108281621e7bf8263644e9d0d
hoangpm96 <hoangpm.qn96@gmail.com>
2016-12-24 11:13:36 +0700

    Draw Door OK 2

--- CastleVania/Player.cpp
+++ CastleVania/Player.cpp
@@ -1002,7 +1002,7 @@ void Player::Collision(list<GameObject*> &obj, float dt) {
 							if (_hasKnockBack) // nếu knockback thì thoát knockback khi chạm Movingplatform
 							{
 								_hasKnockBack = false;
-								vY = 0;
+								vY = -SPEED_Y;
 								vX = 0;
 								_a = 0;
 								_allowPress = true;
