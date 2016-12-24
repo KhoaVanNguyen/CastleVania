96a22fa99288edbd4bdbb12925a27550e5b2f444
Khoa Nguyen <vankhoa0603@gmail.com>
2016-12-24 10:37:13 +0700

    Add comment in QBackground

--- CastleVania/QBackground.h
+++ CastleVania/QBackground.h
@@ -17,18 +17,18 @@ using namespace std;
 class QBackground
 {
 	void Load(int, QNode*&);
-	void GetNodeObject(int, int, QNode*);// lấy all các object xuất hiện trên màn hình.
+	void GetObjectsIn(int, int, QNode*);// lấy tất cả các object xuất hiện trên màn hình.
 public:
 	QNode *tree;
 	GSprite* bgSprite;
-	map<int, Tile*> *_myObject;
-	map<int, QNode*> *_nodeOfTree;
-	list<int> *_listTile;
+	map<int, Tile*> *listObjects; // tất cả tiles từ file
+	map<int, QNode*> *nodes; // tổng số nodes 
+	list<int> *currentTiles; // danh sách các tile xuất hiện trong màn hình để vẽ
 
 	QBackground(void);
 	QBackground(int);
-	void LoadTree();
-	void GetTreeObject(int, int);
+	void LoadQuadTreeFromFile(); // Load quadtree từ file
+	void GetAvailableTiles(int, int); // Lấy danh sách các tiles vào biến currentTiles
 	void Draw(GCamera *camera);
 	~QBackground(void);
 };
