3bc863379265e8028fdfab20c3d616239f5665a4
hoangpm96 <hoangpm.qn96@gmail.com>
2017-01-02 12:06:13 +0700

    Fix EndGameOk

--- CastleVania/SceneEndGame.cpp
+++ CastleVania/SceneEndGame.cpp
@@ -9,32 +9,21 @@ void SceneEndGame::_initialize()
 	_localTime = 0;
 	_currentSelection = 0;
 	_menuOption = (char**)malloc(2 * sizeof(char*));
-	//_menuOption[0] = "Game Over;
-	_menuOption[0] = "Play Again";
+	_menuOption[0] = "CONTINUE";
 	_menuOption[1] = "EXIT";
 }
 
 void SceneEndGame::_draw()
 {
-
-	_bigFont->renderAnimation("Game Over", 180, 210, 15);
 	switch (_currentSelection)
 	{
 	case 0:
-		_smallFont->renderAnimation(_menuOption[0], 180, 285, 15);
-		_smallFont->render(_menuOption[1], 240, 267);
-		
-		//_smallFont->render(_menuOption[2], 243, 320);
+		_bigFont->renderAnimation(_menuOption[0], 180, 272, 15);
+		_smallFont->render(_menuOption[1], 245, 313);
 		break;
-
-		//_bigFont->renderAnimation(_menuOption[0], 220, 252, 15);
-		//_smallFont->render(_menuOption[1], 215, 293);
-		//_smallFont->render(_menuOption[2], 243, 320);
-		//break;
 	case 1:
-		_smallFont->render(_menuOption[0], 240, 267);
-		//_smallFont->render(_menuOption[1], 215, 293);
-		_smallFont->renderAnimation(_menuOption[1], 230, 315, 15);
+		_smallFont->render(_menuOption[0], 215, 287);
+		_bigFont->renderAnimation(_menuOption[1], 225, 305, 15);
 		break;
 		
 	default:
@@ -109,9 +98,6 @@ void SceneEndGame::LoadResources(LPDIRECT3DDEVICE9 d3ddv)
 
 	_smallFont = new Font(d3ddv, 22, G_ScreenWidth, G_ScreenHeight);
 	_bigFont = new Font(d3ddv, 40, G_ScreenWidth, G_ScreenHeight);
-
-	//Sound::GetInst()->RemoveAllBGM();
-	//Sound::GetInst()->PlayBGSound(EBGSound::EMenuSound);
 }
 
 SceneEndGame::~SceneEndGame(void)
